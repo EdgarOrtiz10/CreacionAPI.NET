76cd6e928de07297f2752d5af856e69284431f1c
EdgarOrtiz10 <2120eaoe@gmail.com>
2023-05-13 09:23:15 -0500

    Creacion Interface Categoria

---
+++ ApiPeliculas/Repositorio/IRepositorio/ICategoriaRepositorio.cs
@@ -0,0 +1,31 @@
+﻿using ApiPeliculas.Modelos;
+
+namespace ApiPeliculas.Repositorio.IRepositorio
+{
+    public interface ICategoriaRepositorio
+    {
+        // Método para obtener todas las categorías
+        ICollection<Categoria> GetCategorias();
+
+        // Método para obtener una categoría por su ID
+        Categoria GetCategoria(int categoriaId);
+
+        // Método para verificar si existe una categoría por su nombre
+        bool ExisteCategoria(string nombre);
+
+        // Método para verificar si existe una categoría por su ID
+        bool ExisteCategoria(int id);
+
+        // Método para crear una nueva categoría
+        bool CrearCategoria(Categoria categoria);
+
+        // Método para actualizar una categoría existente
+        bool ActualizarCategoria(Categoria categoria);
+
+        // Método para borrar una categoría existente
+        bool BorrarCategoria(Categoria categoria);
+
+        // Método para guardar los cambios realizados en el repositorio
+        bool Guardar();
+    }
+}
