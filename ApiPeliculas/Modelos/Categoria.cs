8d13e48929756156690aa553d8878ec6cfdbf09a
EdgarOrtiz10 <2120eaoe@gmail.com>
2023-05-12 19:59:54 -0500

    Modelo Categoria

---
+++ ApiPeliculas/Modelos/Categoria.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiPeliculas.Modelos // Define el espacio de nombres ApiPeliculas.Modelos
+{
+    public class Categoria // Define una clase llamada Categoria
+    {
+        [Key] // Indica que la siguiente propiedad es clave primaria
+        public int Id { get; set; } // Define una propiedad pública llamada Id de tipo entero
+
+        [Required] // Indica que la siguiente propiedad es obligatoria
+        public string MyProperty { get; set; } // Define una propiedad pública llamada MyProperty de tipo cadena de caracteres
+
+        public DateTime FechaCreacion { get; set; } // Define una propiedad pública llamada FechaCreacion de tipo DateTime
+    }
+}
