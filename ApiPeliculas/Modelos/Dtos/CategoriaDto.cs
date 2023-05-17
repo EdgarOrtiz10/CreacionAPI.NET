d5324c99d4d42d393e3d1bbcd9a48ccda52f98de
EdgarOrtiz10 <2120eaoe@gmail.com>
2023-05-16 21:10:32 -0500

    DTO para Categoría

---
+++ ApiPeliculas/Modelos/Dtos/CategoriaDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;  // Importar el espacio de nombres System.ComponentModel.DataAnnotations
+
+namespace ApiPeliculas.Modelos.Dtos
+{
+    public class CategoriaDto
+    {
+        //Este código también es parte de una API de películas y la clase CategoriaDto se utiliza para representar una categoría en la API.
+        //Contiene información sobre el ID de la categoría y el nombre de la misma, con validaciones para asegurarse de que el nombre no esté
+        //vacío y no exceda los 60 caracteres.
+        public int Id { get; set; }  // Define una propiedad pública llamada Id de tipo entero
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]  // Indica que la siguiente propiedad es obligatoria
+        [MaxLength(60, ErrorMessage = "El número máximo de caracteres es de 60!")]  // Establece la longitud máxima permitida para la propiedad
+        public string Nombre { get; set; }  // Define una propiedad pública llamada Nombre de tipo cadena de caracteres
+    }
+}
---
+++ ApiPeliculas/Modelos/Dtos/CrearCategoriaDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiPeliculas.Modelos.Dtos
+{
+    public class CrearCategoriaDto
+    {
+        //Este código es parte de una API de películas y el objetivo de esta clase es representar un DTO
+        //(Objeto de Transferencia de Datos) utilizado para crear una nueva categoría en la API.
+
+        //Esta validación es importante sino se crea vacia el nombre de la categoria 
+        [Required(ErrorMessage = "El nombre es obligatorio")] // Indica que la siguiente propiedad es obligatoria
+        [MaxLength(60, ErrorMessage = "El numero maximo de caracteres es de 60!")]
+        public string Nombre { get; set; } // Define una propiedad pública llamada MyProperty de tipo cadena de caracteres
+    }
+}
