5937c91dde16cd22713b0ba0b78133b570741f56
EdgarOrtiz10 <2120eaoe@gmail.com>
2023-05-16 21:24:05 -0500

    Configuración de Extensión AutoMapper

---
+++ ApiPeliculas/PeliculasMappers/PeliculasMapper.cs
@@ -0,0 +1,19 @@
+﻿using ApiPeliculas.Modelos;  // Importar el espacio de nombres ApiPeliculas.Modelos
+using ApiPeliculas.Modelos.Dtos;  // Importar el espacio de nombres ApiPeliculas.Modelos.Dtos
+using AutoMapper;  // Importar el espacio de nombres AutoMapper
+
+namespace ApiPeliculas.PeliculasMapper
+{
+    //En resumen, este código define una clase llamada PeliculasMapper que hereda de la clase Profile proporcionada por AutoMapper.
+    //En el constructor de la clase, se configuran los mapeos entre las clases Categoria, CategoriaDto y CrearCategoriaDto, permitiendo
+    //la conversión entre objetos de dominio y objetos utilizados para la transferencia de datos. Esto facilita el proceso de mapeo y
+    //transformación de datos en la API de películas.
+    public class PeliculasMapper : Profile //Se define la clase PeliculasMapper que hereda de la clase Profile proporcionada por AutoMapper.
+    {
+        public PeliculasMapper()
+        {
+            CreateMap<Categoria, CategoriaDto>().ReverseMap();  // Crear un mapeo bidireccional entre Categoria y CategoriaDto
+            CreateMap<Categoria, CrearCategoriaDto>().ReverseMap();  // Crear un mapeo bidireccional entre Categoria y CrearCategoriaDto
+        }
+    }
+}
